5422681d4d88c17c97c9cf990443c881fb76ae82
tanverrana <tanversobur@gmail.com>
2022-04-09 05:31:11 +0600

    create login and register components

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
-const Login = () => {
+const Register = () => {
     return (
         <div>
-            <h2>Please login</h2>
-            <div style={{ margin: "20px" }}>
-                <button>Google Sign In</button>
-            </div>
+            <h2>Please Register!!</h2>
             <form>
+                <input type="text" placeholder="Your Name" /><br />
                 <input type="email" placeholder="Your email" /><br />
                 <input type="password" name="" id="" placeholder="password" /><br />
-                <input type="submit" value="Login" />
+                <input type="submit" value="Register" />
             </form>
         </div>
     );
 };
 
-export default Login;
+export default Register;

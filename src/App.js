ca3826bcb3f02da57aea854c9c7f4b3f97bbdf40
tanverrana <tanversobur@gmail.com>
2022-04-09 19:59:50 +0600

    orders component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './components/Login/Login';
 import Header from './components/Header/Header';
 import Register from './components/Register/Register';
 import Products from './components/Products/Products';
+import Orders from './components/Orders/Orders';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/products" element={<Products></Products>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/orders" element={<Orders></Orders>}></Route>
 
       </Routes>
     </div>
